7025288f2d3d961aa37a7fe48dc65d87c4be5986
gauripatil9423-ui <gauripatil9423@gmail.com>
2025-11-04 09:38:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function handleFormSubmit(event) {
-  event.preventDefault();
-  alert("Form submitted! (Connect Formspree or EmailJS for real messages)");
-  event.target.reset();
-  return false;
-}
